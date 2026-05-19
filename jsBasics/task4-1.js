a3bd52ed29d5c6162d2cb89b2fc21b897f80160f
Oleksandra Babii <alexababiy118@gmail.com>
2026-05-19 12:37:00 +0300

    feat: add jsBasics: variables, strings, numbers, calculations

---
+++ jsBasics/task4-1.js
@@ -0,0 +1,4 @@
+const radius = 5;
+const circleArea = Math.PI * radius ** 2;
+
+console.log(`Circle area: ${circleArea.toFixed(2)}`);
---
+++ jsBasics/task4-2.js
@@ -0,0 +1,5 @@
+const length = 7;
+const width = 4;
+const rectangleArea = length * width;
+
+console.log(`Rectangle area: ${rectangleArea.toFixed(2)}`);
